f42d1488fe812f36a7557acb8160c48979fa8599
ParvinSha <parvin.shafiee.m@gmail.com>
2025-04-14 13:50:52 +0200

    #12-Responsive design for all pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,22 @@ import { ShoppingCart } from 'lucide-react'
 export default function Home() {
   return (
     <main
-      className="relative min-h-screen flex flex-col items-center pt-4 sm:pt-8 p-4 sm:p-6 md:p-8 bg-cover bg-center"
+      className="relative flex flex-col items-center pt-4 sm:pt-8 p-4 sm:p-6 md:p-8 bg-cover bg-center md:min-h-screen"
       style={{ backgroundImage: "url('/images/shopping-background.jpg')" }}
     >
-      <div className="flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 text-center">
-        <h1 className="text-3xl sm:text-4xl font-bold mb-12">
-          Welcome to Products Shop App
-        </h1>
-        <a
-          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-lg sm:text-xl h-12 px-6"
-          href="/products"
-        >
-          <ShoppingCart size={24} />
-          Go to Products Shop
-        </a>
+      <div className="flex-grow flex items-center justify-center w-full">
+        <div className="flex flex-col items-center w-full max-w-3xl p-6 sm:p-12 text-center">
+          <h1 className="text-2xl sm:text-4xl font-bold mb-8 text-white">
+            Welcome to Products Shop App
+          </h1>
+          <a
+            className="inline-flex items-center justify-center gap-2 px-4 sm:px-6 py-2 sm:py-3 rounded-3xl border border-transparent bg-foreground text-background hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base md:text-lg transition-colors"
+            href="/products"
+          >
+            <ShoppingCart size={18} />
+            Go to Products Shop
+          </a>
+        </div>
       </div>
     </main>
   )
